45e6d48bb39c42d09810e5d6f14059e243dba55e
alexnguyen2201 <n.vanson.2201@gmail.com>
2024-02-27 22:35:05 +0700

    update name function to calculate_height

--- Algorithms/Graph/BinaryTree/find_heigh_of_binary_tree.py
+++ Algorithms/Graph/BinaryTree/find_heigh_of_binary_tree.py
@@ -1,8 +1,8 @@
-def cal_height(node):
+def calculate_height(node):
     if not node:
         return 0
 
-    left = cal_height(node.left)
-    right = cal_height(node.right)
+    left = calculate_height(node.left)
+    right = calculate_height(node.right)
 
     return max(left, right) + 1
